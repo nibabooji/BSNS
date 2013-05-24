60f08f3baa6c0d5737fac362e97ddfdbd3789566
sunghyun <shtjdgus1090@naver.com>
2013-05-24 14:52:07 +0900

    dd

--- BSNS/src/main/java/org/bsns/server/domain/MemberVO.java
+++ BSNS/src/main/java/org/bsns/server/domain/MemberVO.java
@@ -2,4 +2,62 @@ package org.bsns.server.domain;
 
 public class MemberVO {
 
+	private String email;
+	
+	private String dept;
+	
+	private String position;
+	
+	private String password;
+	
+	private String name;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "MemberVO [email=" + email + ", dept=" + dept + ", position="
+				+ position + ", password=" + password + ", name=" + name + "]";
+	}
+	
+	
+	
 }
