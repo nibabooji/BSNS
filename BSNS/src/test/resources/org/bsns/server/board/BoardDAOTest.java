0a95e6441c82f48d51e66d0e3e23b2bcf64f28ca
KimYM <srdudals@naver.com>
2013-05-24 23:05:43 +0900

    boardDAO Test

---
+++ BSNS/src/test/resources/org/bsns/server/board/BoardDAOTest.java
@@ -0,0 +1,62 @@
+package org.bsns.server.board;
+
+import java.sql.SQLException;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.bsns.server.common.Condition;
+import org.bsns.server.common.JunitSettting;
+import org.bsns.server.domain.BoardVO;
+import org.junit.Test;
+
+
+public class BoardDAOTest 
+extends JunitSettting
+{
+
+	@Resource(name="BoardDAO")
+	private BoardDAO boardDao;
+	
+	private Logger log = Logger.getLogger(this.getClass());
+	
+	@Test
+	public void create() throws SQLException{
+		BoardVO board = new BoardVO();
+		
+		board.setType(1);
+		board.setContent("DAO TEST CONTENT");
+		board.setEmail("test5");
+		
+		boardDao.create(board);
+	}
+
+	@Test
+	public void read() throws SQLException{
+		Condition<String, Object> condition = new Condition<String, Object>();
+		
+		condition.put("number", 101);
+		
+		log.debug("BoardDAO read Test result : " + boardDao.read(condition));
+	}
+	
+	@Test
+	public void update() throws SQLException{
+		BoardVO board = new BoardVO();
+		
+		board.setNumber(101);
+		board.setType(1);
+		board.setContent("DAO TEST UPDATE CONTENT");
+		board.setEmail("test5");
+		
+		boardDao.update(board);
+	}
+	@Test
+	public void delete() throws SQLException{
+		Condition<String, Object> condition = new Condition<String, Object>();
+		
+		condition.put("number", 100);
+		
+		boardDao.delete(condition);
+	}
+}
---
+++ BSNS/src/test/resources/org/bsns/server/common/JunitSettting.java
@@ -0,0 +1,14 @@
+package org.bsns.server.common;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={
+		"file:src/main/webapp/WEB-INF/spring/root-context.xml", 
+		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+public class JunitSettting {
+
+}
